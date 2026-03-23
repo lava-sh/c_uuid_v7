4c4c75f27a3475745730aee996393b5ff9d06cc3
chiri <chirizxc@proton.me>
2026-03-24 01:16:10 +0300

    add stdlib uuid to benchmark (#8)

--- benchmark/run.py
+++ benchmark/run.py
@@ -1,8 +1,13 @@
 import platform
 import time
+import uuid
 from collections.abc import Callable
-from importlib.metadata import version as get_lib_version
+from importlib.metadata import (
+    PackageNotFoundError,
+    version as get_lib_version,
+)
 from pathlib import Path
+from typing import TypeAlias
 
 import altair as alt
 import cpuinfo
@@ -23,9 +28,10 @@ N = 500_000
 FILE_PATH = Path(__file__).resolve().parent
 CPU = cpuinfo.get_cpu_info()["brand_raw"]
 PY_VERSION = f"{platform.python_version()} ({platform.system()} {platform.release()})"
+BenchmarkCase: TypeAlias = tuple[Callable, str]
 
 
-def benchmark(func: Callable, count: int) -> float:
+def _benchmark(func: Callable, count: int) -> float:
     start = time.perf_counter()
     for _ in range(count):
         func()
@@ -33,18 +39,29 @@ def benchmark(func: Callable, count: int) -> float:
     return end - start
 
 
-def benchmark_cases(cases: dict[str, Callable], count: int) -> dict[str, float]:
+def benchmark(
+    cases: dict[str, BenchmarkCase],
+    count: int,
+) -> dict[str, float]:
     return {
-        name: benchmark(func, count)
-        for name, func in cases.items()
+        name: _benchmark(func, count)
+        for name, (func, _) in cases.items()
     }  # fmt: skip
 
 
+def get_label(name: str, package_name: str) -> str:
+    try:
+        version = get_lib_version(package_name)
+    except PackageNotFoundError:
+        version = "stdlib"
+    return f"{name}\n{version}"
+
+
 def plot_benchmark(
+    cases: dict[str, BenchmarkCase],
     results: dict[str, float],
     save_path: Path,
     scenario: str,
-    package_names: dict[str, str],
 ) -> None:
     df = pl.DataFrame({
         "implementation": list(results.keys()),
@@ -58,10 +75,7 @@ def plot_benchmark(
     df = df.with_columns(
         pl.Series(
             "implementation_label",
-            [
-                f"{name}\nv{get_lib_version(package_names[name])}"
-                for name in df["implementation"]
-            ],
+            [get_label(name, cases[name][1]) for name in df["implementation"]],
         ),
     )
 
@@ -126,53 +140,37 @@ def plot_benchmark(
 
 
 def run(run_count: int) -> None:
-    default_generator = uuid7gen.UUIDv7()
-
-    default_cases = {
-        "c_uuid_v7": c_uuid_v7.uuid7,
-        "uuid_utils": uuid_utils.uuid7,
-        "fastuuid": fastuuid.uuid7,
-        "uuid_v7": uuid_v7_uuid7,
-        "uuid6": uuid6.uuid7,
-        "lastuuid": lastuuid.uuid7,
-        "UUIDv7gen": default_generator.generate,
-    }
-    default_package_names = {
-        "c_uuid_v7": "c_uuid_v7",
-        "uuid_utils": "uuid-utils",
-        "uuid_v7": "uuid-v7",
-        "uuid6": "uuid6",
-        "fastuuid": "fastuuid",
-        "lastuuid": "lastuuid",
-        "UUIDv7gen": "UUIDv7gen",
+    cases = {
+        "uuid": (uuid.uuid7, "uuid"),
+        "c_uuid_v7": (c_uuid_v7.uuid7, "c_uuid_v7"),
+        "uuid_utils": (uuid_utils.uuid7, "uuid-utils"),
+        "fastuuid": (fastuuid.uuid7, "fastuuid"),
+        "uuid_v7": (uuid_v7_uuid7, "uuid-v7"),
+        "uuid6": (uuid6.uuid7, "uuid6"),
+        "lastuuid": (lastuuid.uuid7, "lastuuid"),
+        "UUIDv7gen": (uuid7gen.UUIDv7().generate, "UUIDv7gen"),
     }
 
     compact_cases = {
-        "c_uuid_v7.compat": c_uuid_v7_compat.uuid7,
-        "uuid_utils.compat": uuid_utils_compat.uuid7,
-        "uuid_v7": uuid_v7_uuid7,
-        "uuid6": uuid6.uuid7,
-        "lastuuid": lastuuid.uuid7,
-    }
-    compact_package_names = {
-        "c_uuid_v7.compat": "c_uuid_v7",
-        "uuid_utils.compat": "uuid-utils",
-        "uuid_v7": "uuid-v7",
-        "uuid6": "uuid6",
-        "lastuuid": "lastuuid",
+        "c_uuid_v7.compat": (c_uuid_v7_compat.uuid7, "c_uuid_v7"),
+        "uuid_utils.compat": (uuid_utils_compat.uuid7, "uuid-utils"),
+        "uuid_v7": (uuid_v7_uuid7, "uuid-v7"),
+        "uuid6": (uuid6.uuid7, "uuid6"),
+        "lastuuid": (lastuuid.uuid7, "lastuuid"),
+        "uuid": (uuid.uuid7, "uuid"),
     }
 
     plot_benchmark(
-        benchmark_cases(default_cases, run_count),
-        FILE_PATH / "uuid7-default.svg",
+        cases,
+        benchmark(cases, run_count),
+        FILE_PATH / "uuid7.svg",
         "uuid7() default APIs",
-        default_package_names,
     )
     plot_benchmark(
-        benchmark_cases(compact_cases, run_count),
+        compact_cases,
+        benchmark(compact_cases, run_count),
         FILE_PATH / "uuid7-compact.svg",
-        "uuid7() compact / stdlib-compatible APIs",
-        compact_package_names,
+        "uuid7() stdlib-compatible APIs",
     )
 
 
