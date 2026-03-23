dd29731fe0180c9f5d610b8f5422120582ea3350
chiri <chirizxc@proton.me>
2026-03-23 04:01:16 +0300

    update

--- src/c_uuid_v7/__init__.py
+++ src/c_uuid_v7/__init__.py
@@ -8,12 +8,12 @@ __all__ = (
 import os
 
 from . import compat
-from ._uuid_v7_c import UUID, _reseed_rng, _uuid7
+from ._uuid_v7_c import (
+    UUID,
+    _reseed_rng,
+    _uuid7 as uuid7,
+)
 from ._version import __version__
 
 if hasattr(os, "fork"):
     os.register_at_fork(after_in_child=_reseed_rng)
-
-
-def uuid7(timestamp: int | None = None, nanos: int | None = None) -> UUID:
-    return _uuid7(timestamp, nanos)
