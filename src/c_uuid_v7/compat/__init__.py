ba0b86f6087e47edaf859130c370fc3f80403804
chiri <chirizxc@proton.me>
2026-03-24 17:59:54 +0300

    update

--- src/c_uuid_v7/compat/__init__.py
+++ src/c_uuid_v7/compat/__init__.py
@@ -1,17 +1,27 @@
+import sys
+import uuid
 from typing import Literal
-from uuid import UUID
 
 from c_uuid_v7.__core import _uuid7
 
-_uuid_from_int = getattr(UUID, "_from_int", None)
+if sys.version_info >= (3, 14):
+
+    def from_int(value: int) -> uuid.UUID:
+        return uuid.UUID._from_int(value)  # noqa: SLF001
+
+else:
+
+    def from_int(value: int) -> uuid.UUID:
+        obj = object.__new__(uuid.UUID)
+        object.__setattr__(obj, "int", value)  # noqa: PLC2801
+        object.__setattr__(obj, "is_safe", None)  # noqa: PLC2801
+        return obj
 
 
 def uuid7(
     timestamp: int | None = None,
     nanos: int | None = None,
     mode: Literal["fast", "secure"] = "fast",
-) -> UUID:
+) -> uuid.UUID:
     uuid7_int = _uuid7(timestamp, nanos, mode).int
-    if _uuid_from_int is not None:
-        return _uuid_from_int(uuid7_int)
-    return UUID(int=uuid7_int)
+    return from_int(uuid7_int)
