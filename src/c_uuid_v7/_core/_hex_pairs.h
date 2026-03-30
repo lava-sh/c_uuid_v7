5af41ae3970b56e2f882e6a92ad0a7325f7c2e94
chiri <chirizxc@proton.me>
2026-03-30 13:09:50 +0300

    refactoring: use PCG random (#20)

--- src/c_uuid_v7/_core/_hex_pairs.h
+++ src/c_uuid_v7/_core/_hex_pairs.h
@@ -6,18 +6,33 @@
 
 extern const uint16_t HEX_PAIRS[256];
 
-#if defined(__GNUC__) || defined(__clang__)
-#define NOT_UNUSED __attribute__((unused))
-#else
-#define NOT_UNUSED
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-function"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
 #endif
 
-static NOT_UNUSED void hex_pair(char *out, const unsigned char byte) {
+#if defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable : 4505)
+#endif
+
+static void hex_pair(char *out, const unsigned char byte) {
     const uint16_t pair = HEX_PAIRS[byte];
 
     memcpy(out, &pair, sizeof(pair));
 }
 
-#undef NOT_UNUSED
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
 
 #endif
