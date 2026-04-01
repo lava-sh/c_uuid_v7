01fb105ecbb2f9a9f0077e1ac41b7c92bcfe2f93
chiri <chirizxc@proton.me>
2026-04-01 04:09:38 +0300

    update

--- tests/test_uuid7.py
+++ tests/test_uuid7.py
@@ -250,7 +250,10 @@ def test_reseed_is_called_when_forking() -> None:
     read_end, write_end = os.pipe()
     c_uuid_v7.uuid7()
 
-    pid = os.fork()  # ty: ignore[unresolved-attribute]
+    fork = getattr(os, "fork", None)
+    assert fork is not None
+
+    pid = fork()
     if pid == 0:
         os.close(read_end)
         next_uuid_child = str(c_uuid_v7.uuid7())
