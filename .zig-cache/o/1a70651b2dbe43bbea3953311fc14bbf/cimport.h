c88389e19b9b2ee9be72587830179ac845e3e341
chiri <chirizxc@proton.me>
2026-04-02 22:04:13 +0300

    fix: use pip build frontend with hpy dependency for wheels

--- .zig-cache/o/1a70651b2dbe43bbea3953311fc14bbf/cimport.h
+++
@@ -1,4 +0,0 @@
-#define PY_SSIZE_T_CLEAN 1
-#include <Python.h>
-#include <_python.h>
-#include <bcrypt.h>
--- .zig-cache/o/8143c97f33b872866ff797c648ac0886/cimport.h
+++
@@ -1,5 +0,0 @@
-#define PY_SSIZE_T_CLEAN 1
-#include <Python.h>
-#define WIN32_LEAN_AND_MEAN 1
-#include <windows.h>
-#include <bcrypt.h>
