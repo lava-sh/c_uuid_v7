76e516eeacb6f4ccce0a34e04687926ad9ff2b21
chiri <chirizxc@proton.me>
2026-04-03 02:00:21 +0300

    update

--- src/hexpairs.c
+++ src/hexpairs.c
@@ -1,10 +1,13 @@
 #include "hexpairs.h"
 
-#if defined(_MSC_VER)
-static_assert(sizeof(uint16_t) == 2U, "uint16_t must be 16-bit");
+#if defined(_MSC_VER) && !defined(__clang__)
+    #include <assert.h>
+    #define CUUID_STATIC_ASSERT static_assert
 #else
-_Static_assert(sizeof(uint16_t) == 2U, "uint16_t must be 16-bit");
+    #define CUUID_STATIC_ASSERT _Static_assert
 #endif
+
+CUUID_STATIC_ASSERT(sizeof(uint16_t) == 2U, "uint16_t must be 16-bit");
 
 #if defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
     #define HEX_U16(a, b) ((uint16_t)((uint16_t)(a) << 8 | (uint16_t)(b)))
