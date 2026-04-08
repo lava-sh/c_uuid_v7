9f75d2f5cd31cc9c40814f20f4c44b570fb05311
chiri <chirizxc@proton.me>
2026-04-08 19:37:27 +0300

    update

--- src/platform.zig
+++ src/platform.zig
@@ -2,8 +2,6 @@ const std = @import("std");
 const builtin = @import("builtin");
 const state = @import("state.zig");
 
-const posix = std.posix;
-
 pub fn nowMs() u64 {
     const instant = std.time.Instant.now() catch unreachable;
     return @as(u64, @intCast(instant.timestamp / std.time.ns_per_ms));
@@ -15,6 +13,6 @@ pub fn fillRandom(buf: []u8) state.Status {
         return .ok;
     }
 
-    posix.getrandom(buf) catch return .random_failure;
+    std.posix.getrandom(buf) catch return .random_failure;
     return .ok;
 }
