414a4fda104b7fda2a83a271edff1f078b4ba9bf
chiri <chirizxc@proton.me>
2026-04-08 20:08:16 +0300

    update

--- src/platform.zig
+++ src/platform.zig
@@ -3,11 +3,11 @@ const builtin = @import("builtin");
 const state = @import("state.zig");
 
 pub fn nowMs() u64 {
-    if (builtin.os.tag == .linux) {
-        return @as(u64, @intCast(@divFloor(std.time.nanoTimestamp(), std.time.ns_per_ms)));
+    if (builtin.os.tag == .windows) {
+        const instant = std.time.Instant.now() catch unreachable;
+        return @as(u64, @intCast(instant.timestamp / std.time.ns_per_ms));
     }
-    const instant = std.time.Instant.now() catch unreachable;
-    return @as(u64, @intCast(instant.timestamp / std.time.ns_per_ms));
+    return @as(u64, @intCast(@divFloor(std.time.nanoTimestamp(), std.time.ns_per_ms)));
 }
 
 pub fn fillRandom(buf: []u8) state.Status {
