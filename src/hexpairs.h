05adb420394af182e78239fd9431dd8ac26439b6
chiri <chirizxc@proton.me>
2026-04-12 17:55:57 +0300

    update

--- src/hexpairs.h
+++ src/hexpairs.h
@@ -6,33 +6,10 @@
 
 extern const uint16_t HEX_PAIRS[256];
 
-#if defined(__clang__)
-    #pragma clang diagnostic push
-    #pragma clang diagnostic ignored "-Wunused-function"
-#elif defined(__GNUC__)
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wunused-function"
-#endif
-
-#if defined(_MSC_VER)
-    #pragma warning(push)
-    #pragma warning(disable : 4505)
-#endif
-
 static void hex_pair(char *out, const unsigned char byte) {
     const uint16_t pair = HEX_PAIRS[byte];
 
     memcpy(out, &pair, sizeof(pair));
 }
-
-#if defined(_MSC_VER)
-    #pragma warning(pop)
-#endif
-
-#if defined(__clang__)
-    #pragma clang diagnostic pop
-#elif defined(__GNUC__)
-    #pragma GCC diagnostic pop
-#endif
 
 #endif
