ba272212265d73f1c370af046074a4590b0cba36
chiri <chirizxc@proton.me>
2026-04-19 14:32:47 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -288,7 +288,7 @@ class ZigBuildExt(build_ext):
         zig = ci_zig or shutil.which("python-zig")
 
         if IS_MACOS:
-            logger.info("🍏 macOS detected: using Clang toolchain")
+            logger.info("🍎 MACOS detected: using Clang toolchain")
             logger.info(
                 "🍏 MACOSX_DEPLOYMENT_TARGET=%s",
                 os.environ.get("MACOSX_DEPLOYMENT_TARGET"),
